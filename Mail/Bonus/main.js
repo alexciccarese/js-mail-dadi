bc842df2b116ad2b9fbc17661b6c9074593c18b1
alexciccarese <alex.ciccarese@gmail.com>
2025-02-13 15:22:49 +0100

    Add bonus email first commit

---
+++ Mail/Bonus/main.js
@@ -0,0 +1,8 @@
+/* Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */
+
+let emailList = ['lista1@example.com', 'lista2@example.com', 'lista3@example.com', 'lista4@example.com', 'lista5@example.com']
+let userEmail = prompt('Inserisci qui la tua mail: es. name.s@gmail.com')
+
+
